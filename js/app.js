fdde442f2f83aa8e2737b0407dc9b9a1a39165bf
ABiviano13 <abiviano0@outlook.com>
2023-02-01 16:22:53 +0100

    FEAT variabili km age

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 // 1. Definire le variabili km e age
+const formBtnElement = document.querySelector(
+    '.card-generator input[type="button"]'
+);
+let km = document.querySelector(
+    '.card-generator input[name="km"]'
+);
+let age = document.querySelector(
+    '.card-generator select[name="age"]'
+);
+console.log (km, age);
 // 2. Inserire nell'input km i kilometri da percorrere (0.21 al km)
-// 3. Applicare i vari sconti per minorenni e over 65 - function swicth select
+const priceKm = '0,21';
+priceBasis = km * priceKm;
+console.log (priceKm, priceBasis);
+// 3. Applicare i vari sconti per minorenni e over 65 
+let minors = document.getElementById('minors');
+let elderly = document.getElementById('elderly');
 // 4. al click bisogna generare il prezzo totale 
