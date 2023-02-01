8bca357cc59c50b0fc8fcbfa1c1be5225b3f909a
ABiviano13 <abiviano0@outlook.com>
2023-02-01 16:03:56 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// 1. Definire le variabili km e age
+// 2. Inserire nell'input km i kilometri da percorrere (0.21 al km)
+// 3. Applicare i vari sconti per minorenni e over 65 - function swicth select
+// 4. al click bisogna generare il prezzo totale 
