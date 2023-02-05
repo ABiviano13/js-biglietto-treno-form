bfb6dba9559d860e5c3d823dc2230ae4eefadac4
ABiviano13 <abiviano0@outlook.com>
2023-02-05 17:05:11 +0100

    COMPLETED et CORRECT

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 // 1. Definiamo le variabili km (input)
 const km = document.getElementById('km');
 console.log (km);
+const resultKm = document.getElementById('input-result-km');
 
 const priceKm = 0.21;
 console.log (priceKm);
@@ -9,6 +10,7 @@ console.log (priceKm);
 // 2. Definiamo le variabili age (input)
 const age = document.getElementById('age');
 console.log(age);
+const resultAge = document.getElementById('input-result-age');
 
 // 3. Definiamo la variabile prezzo totale
 const resultPrice = document.getElementById('pricetotal');
@@ -20,8 +22,15 @@ const formbutton = document.querySelector(
 );
 console.log(formbutton);
 
+// 5. Definiamo variabili risultato
+const firstNameLastName = document.getElementById('firstname-lastname');
+const resultName = document.getElementById('input-result-name');
+const resultNameTicket = document.getElementById('input-result-name2');
+const codiceCp = document.getElementById('codicecp');
+
   // - Al click sul pulsante si deve generare il prezzo totale
 formbutton.addEventListener('click', function () {
+
     let kmValue = km.value;
     kmValue = parseFloat(kmValue);
 
@@ -32,20 +41,29 @@ formbutton.addEventListener('click', function () {
     let priceTotal = age.value;
 
     switch (priceTotal) {
-        case 'minors':
+        case 'minorenne':
             let minorsDiscout = priceBasis * 20 / 100;
             priceTotal = priceBasis - minorsDiscout;
             break
-        case 'adults':
+        case 'maggiorenne':
             priceTotal = priceBasis;   
             break 
-        case 'elderly':
+        case 'over65':
             let elderlyDiscout = priceBasis * 40 / 100;
             priceTotal = priceBasis - elderlyDiscout; 
             break   
     };
     console.log(priceTotal);
 
-    resultPrice.innerHTML = priceTotal;
+    const randomCodiceCp = Math.random();
+
+    let nameValue = firstNameLastName.value;
+
+    resultName.innerHTML = nameValue;
+    resultNameTicket.innerHTML = nameValue;
+    resultKm.innerHTML = kmValue;
+    resultAge.innerHTML = age.value;
+    resultPrice.innerHTML = priceTotal.toFixed(2);
+    codiceCp.innerHTML = randomCodiceCp.toFixed(5);
 });
 // 5. Stampiamo tutto sul nostro file html
